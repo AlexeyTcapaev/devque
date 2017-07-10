b892c7ff7078f9e67a2287c980173493ca17548f
AlexeyTcapaev <lexx1995@mail.ru>
2017-07-10 23:56:00 +0300

    progress bar 1.0

--- tdmerkuriy/progress.js
+++ tdmerkuriy/progress.js
@@ -4,19 +4,20 @@ $(document).ready(function () {
 // progressbar.js@1.0.0 version is used
 // Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
 
+
 var bar = new ProgressBar.Circle(circle2, {
   color: '#3c4761',
   // This has to be the same size as the maximum width to
   // prevent clipping
-  strokeWidth: 4,
-  trailWidth: 1,
+  strokeWidth: 8,
+  trailWidth: 8,
   easing: 'easeInOut',
   duration: 1400,
   text: {
     autoStyleContainer: false
   },
-  from: { color: '#dfe8ed', width: 1 },
-  to: { color: '#30bae7', width: 4 },
+  from: { color: '#dfe8ed', width: 8 },
+  to: { color: '#30bae7', width: 8 },
   // Set default step function for all animate calls
   step: function(state, circle) {
     circle.path.setAttribute('stroke', state.color);
@@ -32,32 +33,33 @@ var bar = new ProgressBar.Circle(circle2, {
   }
 });
 bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
-bar.text.style.fontSize = '3rem';
-bar.animate(1.0);  // Number from 0.0 to 1.0
+bar.text.style.fontSize = '3vw';
+bar.animate(1.0);  
+ // Number from 0.0 to 1.0
 
 var bar1 = new ProgressBar.Circle(circle1, {
   color: '#3c4761',
   // This has to be the same size as the maximum width to
   // prevent clipping
-  strokeWidth: 4,
-  trailWidth: 1,
+  strokeWidth: 8,
+  trailWidth: 8,
   easing: 'easeInOut',
   duration: 1400,
   text: {
     autoStyleContainer: false
   },
-  from: { color: '#dfe8ed', width: 1 },
-  to: { color: '#30bae7', width: 4 },
+  from: { color: '#dfe8ed', width: 8 },
+  to: { color: '#30bae7', width: 8 },
   // Set default step function for all animate calls
   step: function(state, circle) {
-      
     circle.path.setAttribute('stroke', state.color);
     circle.path.setAttribute('stroke-width', state.width);
+
     var value = Math.round(circle.value() * 100);
-       if (value === 48) 
-       {
-           
-       }
+      if(circle.value() === 48)
+          {
+              console.log("48");
+          }
     if (value === 0) {
       circle.setText('');
     } else {
@@ -67,7 +69,6 @@ var bar1 = new ProgressBar.Circle(circle1, {
   }
 });
 bar1.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
-bar1.text.style.fontSize = '3rem';
-
-bar1.animate(1.0);  // Number from 0.0 to 1.0
+bar1.text.style.fontSize = '3vw';
+bar1.animate(0.5);  // Number from 0.0 to 1.0
 });
