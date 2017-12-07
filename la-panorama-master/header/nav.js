19896643094ab037caead79169704e33dc7595b7
AlexeyTcapaev <lexx1995@mail.ru>
2017-12-07 03:13:30 +0300

    123

--- la-panorama-master/header/nav.js
+++ la-panorama-master/header/nav.js
@@ -1,7 +1,20 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav();
-    $("nav").sticky({topSpacing:0});
-    jQuery( ".close" ).click(function() {
-      $('.sidenav').sidenav('close');
-    });
+function check() {
+  if (window.innerWidth <= 992) {
+    document.querySelector(".mobile-phone ").style.display = "flex";
+  }
+  else
+  {
+    document.querySelector(".mobile-phone ").style.display = "none";
+  }
+}
+$(document).ready(function () {
+  check();
+  $(window).resize(check);
+  $('.sidenav').sidenav();
+  $("nav").sticky({
+    topSpacing: 0
   });
+  jQuery(".close").click(function () {
+    $('.sidenav').sidenav('close');
+  });
+});
