8c36c29eca34adb483e71b9fd2730bedee00ab54
AlexeyTcapaev <lexx1995@mail.ru>
2018-02-25 23:03:54 +0300

    y5y45

--- dvq/about/about.js
+++ dvq/about/about.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
        $('.sidenav').sidenav();
-       $('ul.grid li img ').width(document.body.clientWidth/8)
+       $('ul.grid li img ').width(document.body.clientWidth/6)
        $(window).resize(function(){
-        $('ul.grid li img ').width(document.body.clientWidth/8)
+        $('ul.grid li img ').width(document.body.clientWidth/6)
        });
 });
