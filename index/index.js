a2e593d89a22d5ce628b4c18a7145e3109752795
AlexeyTcapaev <lexx1995@mail.ru>
2018-03-02 02:05:42 +0300

    14

--- index/index.js
+++ index/index.js
@@ -1,14 +1,4 @@
 $( document ).ready(function() {
-    $('.portfolio-carousel').slick({
-        dots: false,
-        speed: 300,
-        slidesToShow: 1,
-        autoplay: true,
-        autoplaySpeed: 2500,
-        variableWidth: true,
-        focusOnSelect: false,
-      });
-       $('.sidenav').sidenav();
-       $('.modal').modal();
-       $('.chips').chips();
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
 });
