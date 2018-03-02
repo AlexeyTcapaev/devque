5a5ece7ceeef849c0fd9b2412c0928d8176163b7
AlexeyTcapaev <lexx1995@mail.ru>
2018-03-02 19:46:01 +0300

    3555234

--- bizakula/index/index.js
+++ bizakula/index/index.js
@@ -1,4 +1,15 @@
-$( document ).ready(function() {
+$(document).ready(function () {
     $('.sidenav').sidenav();
     $('.parallax').parallax();
+    $("nav").sticky({
+        topSpacing: 0
+    });
+    $('nav').on('sticky-start', function () {
+        $("nav").css("background-color","rgba(39,57,159,1)")
+        console.log("s")
+    });
+    $('nav').on('sticky-end', function () {
+        $("nav").css("background-color","rgba(39,57,159,0)")
+        console.log("e")
+    });
 });
