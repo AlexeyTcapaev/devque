2d211c719b54db9bd713d7208dd2ee426ee8f18c
AlexeyTcapaev <lexx1995@mail.ru>
2018-03-12 01:06:51 +0300

    4122

--- bizakula/index/index2.js
+++ bizakula/index/index2.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
-    $("nav").sticky({
-        topSpacing: 0
-    });
-    $('nav').on('sticky-start', function () {
-        $("nav").css("background-color","rgba(39,57,159,1)")
-        console.log("s")
-    });
-    $('nav').on('sticky-end', function () {
-        $("nav").css("background-color","rgba(39,57,159,0)")
-        console.log("e")
+    $('#homepage').fullpage({
+        scrollingSpeed: 1000,
+        autoScrolling: true,
+        fitToSection: true,
+        fitToSectionDelay: 2000,
+        anchors: ['home', 'about-us', 'what-we-do', 'our-work'],
+        sectionsColor: ['#f2f2f2', '#1BBC9B', '#7E8F7C', '#C63D0F'],
+        verticalCentered: false,
+        navigation: true,
+        navigationPosition: 'right',
+        navigationTooltips: ['Eureka Mediatech', 'About Us', 'What we do', 'Our Work'],
+        responsiveWidth: 900,
     });
 });
---
+++ bizakula/index/index3.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+    $('#wrapper').fullpage({
+        scrollSpeed: 400,
+    });
+});
