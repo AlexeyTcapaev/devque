81d2e28eedb2edc5c2f759d48bbcc25de1a4a7d4
AlexeyTcapaev <lexx1995@mail.ru>
2018-04-08 19:01:42 +0300

    index ready

--- zavod/index/index.js
+++ zavod/index/index.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
+  $(".modal").modal();
+  if (document.documentElement.clientWidth <= 768) $("aside").sidenav();
   let fullheights = document.querySelectorAll(".full-height");
   let a = 1;
   let b = 3;
   for (let index = 0; index < fullheights.length; index++) {
-    let a = $(fullheights[index]).index() / 2;
+    if (document.documentElement.clientWidth > 1024)
+      a = $(fullheights[index]).index() / 2;
+    else a = $(fullheights[index]).index();
     if (a <= 0) a = 1;
     fullheights[index].style.gridRow = a + "/" + (a + 2);
   }
