0c5f6d66a42e48d7a4ccf76c07899c68768a1fae
AlexeyTcapaev <lexx1995@mail.ru>
2018-04-17 01:02:30 +0300

    12424

--- flowers/index/index.js
+++ flowers/index/index.js
@@ -2,21 +2,36 @@ $(document).ready(function() {
   $(".bouquets").click(function() {
  
     if ($("#bouquets").is(":visible")) {
+      $('.dropdown-trigger').dropdown('close');
       console.log(1)
     } else {
       console.log(2)
+      $('.dropdown-trigger').dropdown('open');
       $("#bouquets").addClass("grid");
     }
   });
+
+
+  $(".aside-drop").click(function() {
+ 
+    if ($("#bouquets-aside").is(":visible")) {
+      $('.dropdown-trigger').dropdown('close');
+      console.log(1)
+    } else {
+      console.log(2)
+      $('.dropdown-trigger').dropdown('open');
+      $("#bouquets-aside").addClass("grid");
+    }
+  });
+
   $(".modal").modal();
   $(".carousel.carousel-slider").carousel({
     fullWidth: true,
     indicators: false,
     autoplay: true
   });
-
   $(".dropdown-trigger").dropdown({
-
+  
     onCloseEnd:function(){
       $("#bouquets").removeClass("grid")
     }
