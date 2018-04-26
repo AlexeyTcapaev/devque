cbc443a5aeaff326a3fd1cd27f7d881f3bf5893c
AlexeyTcapaev <lexx1995@mail.ru>
2018-04-26 08:14:02 +0300

    intrer demo

---
+++ interview/src/book.vue
@@ -0,0 +1,38 @@
+<template>
+<li class="collection-item">
+    <div class="card gradient-45deg-light-blue-cyan gradient-shadow">
+        <div class="card-content white-text">
+            <span class="card-title">{{elem.title}}</span>
+            <p>{{elem.author}}</p>
+            <hr>
+            <ul>
+                <li :key="key" v-for="(cat,key) in elem.genres">{{cat}}</li>
+            </ul>
+        </div>
+        <div class="card-action">
+            <a href="#!" class="waves-effect waves-light btn gradient-45deg-red-pink"><i class="material-icons">close</i></a>
+            <a href="#!" class="waves-effect waves-light btn gradient-45deg-red-pink"><i class="material-icons">edit</i></a>
+        </div>
+    </div>
+</li>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  props: ["elem"],
+  data() {
+    return {};
+  },
+  mounted() {},
+  computed: {
+    book() {
+      return this.$store.state.library[this];
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
---
+++ interview/src/search.vue
@@ -0,0 +1,48 @@
+<template>
+  <li class="collection-header gradient-45deg-light-blue-cyan">
+    <h4 class="task-card-title">Поиск</h4>
+     <div class="input-field">
+          <input id="search" type="search" required>
+          <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+          <i class="material-icons">close</i>
+        </div>
+</li>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  data() {
+    return {};
+  },
+  mounted() {},
+  computed: {}
+};
+</script>
+
+<style>
+.collection-header {
+  padding: 20px;
+}
+.task-card-title {
+  margin: 0;
+  color: #fff;
+  font-weight: 300;
+}
+.input-field label,
+input {
+  color: #fff;
+}
+input {
+  padding-left: 50px!important;
+  border-bottom: none!important;
+  background-color: none !important;
+}
+i{
+  color :#fff!important;
+}
+.input-field input[type="search"]:focus:not(.browser-default) {
+  background-color: rgba(0, 0, 0, 0) !important;
+  color: #fff;
+}
+</style>
