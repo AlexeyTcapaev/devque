ea723d375352e6849d3c5cbf05979272a0db4d1a
AlexeyTcapaev <lexx1995@mail.ru>
2018-05-06 22:00:44 +0300

    flowers

---
+++ flowers/auth/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $(".modal").modal();
+    $(".carousel.carousel-slider").carousel({
+      fullWidth: true,
+      indicators: true
+    });
+    $(".dropdown-trigger").dropdown({
+      belowOrigin: true
+    });
+    $(".button-collapse").sideNav();
+    $(".sales .carousel").carousel({
+      numVisible: 3
+    });
+  });
+  
